08d9730ce0f01b6f3ca2e3120c269024f923a686
Nam Nguyen <namnh98@gmail.com>
2023-05-16 13:24:29 +0700

    commit init source

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,22 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   plugins: [react()],
   server: { port: 3000 },
+  esbuild: {
+    loader: 'jsx',
+    include: [
+      'src/**/*.jsx',
+      'src/**/*.tsx',
+      'node_modules/**/*.jsx',
+      'node_modules/**/*.tsx',
+      'src/LocalJsxInJsComponent.js',
+      'node_modules/bad-jsx-in-js-component/index.js',
+      'node_modules/bad-jsx-in-js-component/js/BadJSXinJS.js',
+      'node_modules/bad-jsx-in-js-component/ts/index.ts',
+      'node_modules/bad-jsx-in-js-component/ts/BadTSXinTS.ts',
+      'src/**/*.js',
+      'node_modules/**/*.js',
+      'src/**/*.ts',
+      'node_modules/**/*.ts',
+    ],
+  },
 });
